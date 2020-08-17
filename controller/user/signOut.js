a6621e31abf63c81b564dfef5badbdb937916a6d
KimHyeungGyun <gudrbsdlwid@gmail.com>
2020-08-17 17:41:19 +0900

    Add sign-out
    로그아웃 기능 구현
    resolve #12

--- controller/user/signOut.js
+++ controller/user/signOut.js
@@ -1,5 +1,11 @@
 module.exports = {
   get(req, res) {
+    if (req.session.userId) {
+      req.session.destroy();
+      res.redirect('/');
+    } else {
+      res.redirect('/');
+    }
     res.send('Response[GET]: signOut');
   },
 };
