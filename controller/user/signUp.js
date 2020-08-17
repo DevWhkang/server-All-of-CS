b6e20d4da841f220e5421e3b09f987dc7786ee6f
KimHyeungGyun <gudrbsdlwid@gmail.com>
2020-08-17 17:19:55 +0900

    Add signUp
    회원가입 기능을 추가했습니다.
    resolve #11

--- controller/user/signUp.js
+++ controller/user/signUp.js
@@ -1,5 +1,28 @@
+const { users } = require('../../models');
+
 module.exports = {
   post(req, res) {
+    const { email, password } = req.body;
+
+    users.findOne({
+      where: {
+        email,
+      },
+    })
+      .then((userData) => {
+        if (userData) {
+          res.status(409);
+        } else {
+          users.create({
+            email,
+            password,
+          })
+            .then((newUserData) => res.status(200).json(newUserData.id));
+        }
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
     res.send('Response[POST]: signUp');
   },
 };
